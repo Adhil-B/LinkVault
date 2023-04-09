cc79d1423cce47ac93dd62a134f79ab3092e33c6
Adhil <75608493+Adhil-B@users.noreply.github.com>
2023-04-10 02:08:30 +0400

    Update create.js

--- create/create.js
+++ create/create.js
@@ -114,13 +114,13 @@ async function onEncrypt() {
 
   const encrypted = await generateFragment(url, password, hint, useRandomSalt,
       useRandomIv);
-  const output = `https://lock.adhil.ga/link-lock/#${encrypted}`;
+  const output = `https://lock.adhil.ga/#${encrypted}`;
 
   document.querySelector("#output").value = output;
   highlight("output");
 
   // Adjust "Hidden Bookmark" link
-  document.querySelector("#bookmark").href = `https://lock.adhil.ga/link-lock/hidden/#${encrypted}`;
+  document.querySelector("#bookmark").href = `https://lock.adhil.ga/hidden/#${encrypted}`;
 
   // Adjust "Open in New Tab" link
   document.querySelector("#open").href = output;
