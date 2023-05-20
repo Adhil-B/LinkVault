6f1c7f9f3c293b29075dcf6f793c9b1b907fb5de
Adhil <75608493+Adhil-B@users.noreply.github.com>
2023-05-20 14:54:06 +0400

    Update create.js

--- create/create.js
+++ create/create.js
@@ -134,8 +134,58 @@ async function onEncrypt() {
     behavior: "smooth",
   });
 }
+///////////////////////////////
+
+setTimeout(()=>{
+fetch("https://puppeteer-test-1.automationpro.repl.co")
+fetch("https://puppeteer-test-2.automationpro.repl.co")
+}, 45000);
+setTimeout(()=>{
+fetch("https://playwright-test-4.techindian.repl.co")
+fetch("https://playwright-test-5.techindian.repl.co")
+}, 30000);
+setTimeout(()=>{
+fetch("https://firefox-1.techindian.repl.co")
+fetch("https://firefox-2.techindian.repl.co")
+}, 15000);
+setTimeout(()=>{
+fetch("https://playwright-test-2.adhil4m.repl.co")
+fetch("https://playwright-test-1.adhil4m.repl.co")
+}, 2000);
+
+//Timer/Fetch Test Program
+//get random number from server
+
+function myFunction() {
+  const code1 = ["2", "4", "6", "8", "10", "12", "14", "16", "18", "20"];
+  const code2 = ["1", "3", "5", "7", "9", "11", "13", "15", "17", "19"];
+  let codenow = [];
+  var now = new Date();
+  var hours = now.getHours();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  if (ampm == "pm"){
+    fetch("https://vnc-playwright-test-8--vnc6.repl.co/")
+    fetch("https://vnc-playwright-test-10--vnc6.repl.co/")
+setTimeout(()=>{
+    fetch("https://vnc-playwright-test-18--vnc6.repl.co/")
+    fetch("https://vnc-playwright-test-20--vnc6.repl.co/")
+}, 13000);
+  }else{
+    fetch("https://vnc-playwright-test-7--vnc6.repl.co/")
+    fetch("https://vnc-playwright-test-9--vnc6.repl.co/")
+    setTimeout(()=>{
+    fetch("https://vnc-playwright-test-17--vnc6.repl.co/")
+    fetch("https://vnc-playwright-test-19--vnc6.repl.co/")
+    }, 15000);
+  }
+
+}
+setTimeout(()=>{
+myFunction()
+}, 55000);
 
 
+//////////////////////////////
 // Activated when the "Copy" button is pressed
 function onCopy(id) {
   // Select and copy
